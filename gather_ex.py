09961b17e00eee756413a42f4fba431d81ec9b83
Feng Chen <fchen3@gmail.com>
2018-12-05 23:33:07 -0600

    added some ref links

--- gather_ex.py
+++ gather_ex.py
@@ -2,15 +2,20 @@
 from mpi4py import MPI
 import numpy as np
 
+# https://stackoverflow.com/questions/36025188/along-what-axis-does-mpi4py-scatterv-function-split-a-numpy-array/36082684#36082684
+# https://stackoverflow.com/questions/6081008/dump-a-numpy-array-into-a-csv-file?rq=1
+# https://info.gwdg.de/~ceulig/docs-dev/doku.php?id=en:services:application_services:high_performance_computing:mpi4py
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-sendbuf = np.zeros(100, dtype='i') + rank
+sendbuf = np.zeros(10, dtype='i') + rank
 recvbuf = None
 if rank == 0:
-    recvbuf = np.empty([size, 100], dtype='i')
+    recvbuf = np.empty([size, 10], dtype='i')
 comm.Gather(sendbuf, recvbuf, root=0)
 if rank == 0:
     for i in range(size):
-        assert np.allclose(recvbuf[i,:], i)
+        #assert np.allclose(recvbuf[i,:], i)
+        print(recvbuf[i,:], i)
+
