36145a722c971a3e434e19c30b0c3bedd4636ba2
Feng Chen <fchen3@gmail.com>
2018-12-15 13:56:32 -0600

    able to compile c version

--- lf_acc.c
+++ lf_acc.c
@@ -1,82 +1,155 @@
 #include <stdio.h>
+#include <math.h>
 
-#define N 2
+#define N 3
 #define D 5
 #define C 3
 
 typedef float real;
-typedef uint8 tint;
+typedef unsigned short tint;
 // https://www.geeksforgeeks.org/compute-the-minimum-or-maximum-max-of-two-integers-without-branching/
 // https://stackoverflow.com/questions/24529504/find-out-max-min-of-two-number-without-using-if-else
 // http://graphics.stanford.edu/~seander/bithacks.html#IntegerMinOrMax
-tint get_minimum_vec(tint *a, tint, *b, tint *c, int vec_dim)
+real sum_minimum_vec(tint *a, tint *b, tint vec_dim)
 {
-    for (int i=0;i<vec_dim;i++)
-        c[i] = b[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // min(x, y)
+  tint i, c;  
+  real sum;
+  for (i=0;i<vec_dim;i++) {
+        c = b[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // min(x, y)
+        sum += c;
+  }
 }
 
-tint get_maximum_vec(tint *a, tint, *b, tint *c, int vec_dim)
+real sum_maximum_vec(tint *a, tint *b, tint vec_dim)
 {
-    for (int i=0;i<vec_dim;i++)
-        c[i] = b[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // max(x, y)
+  tint i, c;  
+  real sum;  
+  for (i=0;i<vec_dim;i++) {
+        c = b[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // max(x, y)
+        sum += c;
+  }
 }
 
-tint get_non_zeros(tint *a, tint, *b, tint *c, int vec_dim)
+real get_non_zeros_pair(tint *a, tint *b, tint vec_dim)
 {
-    for (int i=0;i<vec_dim;i++)
-        
+    real sum = 0;
+    tint i;
+    for (i=0;i<vec_dim;i++)
+      if (a[i]>0 && b[i]>0)
+           sum += a[i]+b[i];
 }
 
-//real get_sum_vec(real a, real b, real c, int vec_dim)
-//{
-//    real c[];
-//    for (int i=0;i<vec_dim;i++)
-//        if (a[i] > b[i]) c[i] = a[i]+b[i];
-//}
-//
-//real vec_add(real a, real b, real c, int vec_dim)
-//{
-//    for(i=0; i<vec_dim; i++) 
-//        c[i] = a[i]+b[i];
-//}
+tint get_non_zeros_single(tint *a, tint vec_dim)
+{
+    tint count = 0;
+    tint i;
+    for (i=0;i<vec_dim;i++)
+        if (a[i] != 0)
+           count++;
+}
+
+void get_sum_vec(tint *a, tint *b, tint vec_dim)
+{
+  real sum=0;
+  tint i;
+  for (i=0;i<vec_dim;i++)
+        sum += a[i]+b[i];
+}
+
+void vec_add(tint *a, tint *b, tint *c,tint vec_dim)
+{
+  tint i;  
+  for(i=0; i<vec_dim; i++) 
+        c[i] = a[i]+b[i];
+}
+
+tint vec_and(tint *a, tint *b, tint *c,tint vec_dim)
+{
+  tint i;  
+  for(i=0; i<vec_dim; i++) 
+        c[i] = a[i] & b[i];
+}
+
+tint vec_or(tint *a, tint *b, tint *c,tint vec_dim)
+{
+  tint i;  
+  for(i=0; i<vec_dim; i++) 
+        c[i] = a[i] | b[i];
+}
+
+real vec_norm(tint *a, tint vec_dim)
+{
+    tint i;
+    real norm=0;
+    for(i=0;i<vec_dim; i++) {
+        norm += a[i]*a[i];
+    }
+    return sqrt(norm);
+}
+
+real vec_dot(tint *a, tint *b, tint vec_dim)
+{
+    tint i;
+    real sum=0;
+    for(i=0;i<vec_dim; i++) {
+        sum += a[i]*b[i];
+    }
+    return sum;
+}
 
 int main(void)
 {
     
     tint data[N][D] = {{1,2,3,4,5},{6,7,8,9,10},{7,8,9,10,11}};
-    tint cmb[C][2]={{0,1},{0,2},{1,2}}
-    float data_sum[N];
-    int data_jac[N][D]={0};
+    tint data_jac[N][D]={0};
+    tint cmb[C][2]={{0,1},{0,2},{1,2}};
+    real data_sum[N], a_sum, b_sum, one_data_norm[N], one_an, one_bn, result;
 
-    for (int i=0;i<N;i++)
+    tint i, j, idx_a, idx_b;
+    tint *a, *b, *a_jac, *b_jac;
+    //tint a[D], b[D], a_jac[D], b_jac[D];
+    tint non_zeros[D]={0};
+    tint summed_array[D]={0};
+    real num_sim, numerator_jac, denomenator_jac, numerator_gen_jac, denomenator_gen_jac;
+        
+    for (i=0;i<N;i++) {
         data_sum[i] = 0;
-        for (int j=0;j<D;j++)
-            if (data[i][j]>0) data_jac[i][j]=1;
-            data_sum[i] += data[i][j]
+        for (j=0;j<D;j++) {
+            if (data[i][j]>0) 
+                data_jac[i][j]=1;
+            data_sum[i] += data[i][j];
+        }
+        one_data_norm[i] = 1.0/vec_norm(data[i], D);
+    }
 
-    int idx_a, idx_b;
-    int non_zeros[D]={0};
-    
-    for (int i=0;i<N;i++) {
+    for (i=0;i< C;i++) {
         idx_a = cmb[i][0], idx_b = cmb[i][1];
 
-        a = data[idx_a]
-        a_sum = data_sum[idx_a]
-        a_jac = data_jac[idx_a]
-        b = data[idx_b]
-        b_sum = data_sum[idx_b]
-        b_jac = data_jac[idx_b]
+        a = data[idx_a];
+        a_sum = data_sum[idx_a];
+        a_jac = data_jac[idx_a];
+        b = data[idx_b];
+        b_sum = data_sum[idx_b];
+        b_jac = data_jac[idx_b];
 
-        for (int j=0;j<D;j++)
+        //vec_add(a, b, summed_array, D);
+        numerator_jac = sum_minimum_vec(a_jac, b_jac, D);
+        denomenator_jac = sum_maximum_vec(a_jac, b_jac, D);
+        numerator_gen_jac = sum_minimum_vec(a, b, D);
+        denomenator_gen_jac = sum_maximum_vec(a, b, D);
+        num_sim = get_non_zeros_pair(a, b, D);
+        one_an = one_data_norm[idx_a];
+        one_bn = one_data_norm[idx_b];
+        result = 1.0 - vec_dot(a,b,D)*one_an*one_bn;
             
-        non_zeros = (a >0) & (b > 0)
-        summed_array = a + b
-
-        numerator_jac = np.sum(np.minimum(a_jac,b_jac))
-        denomenator_jac = np.sum(np.maximum(a_jac,b_jac))
-        numerator_gen_jac =np.sum(np.minimum(a,b))
-        denomenator_gen_jac =np.sum(np.maximum(a,b))
-        num_sim = np.sum(summed_array[non_zeros])
-        result = 1 - spatial.distance.cosine(a, b)
+        //non_zeros = (a >0) & (b > 0)
+        //summed_array = a + b
+        //
+        //numerator_jac = np.sum(np.minimum(a_jac,b_jac))
+        //denomenator_jac = np.sum(np.maximum(a_jac,b_jac))
+        //numerator_gen_jac =np.sum(np.minimum(a,b))
+        //denomenator_gen_jac =np.sum(np.maximum(a,b))
+        //num_sim = np.sum(summed_array[non_zeros])
+        //result = 1 - spatial.distance.cosine(a, b)
     }
 }
