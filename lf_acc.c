afb225f4ba8a7a7b9409563e08c990689fb9a461
Feng Chen <fchen3@gmail.com>
2018-12-16 00:04:50 -0600

    value match for simple case

--- lf_acc.c
+++ lf_acc.c
@@ -5,38 +5,48 @@
 #define D 5
 #define C 3
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+
 typedef float real;
 typedef unsigned short tint;
+
 // https://www.geeksforgeeks.org/compute-the-minimum-or-maximum-max-of-two-integers-without-branching/
 // https://stackoverflow.com/questions/24529504/find-out-max-min-of-two-number-without-using-if-else
 // http://graphics.stanford.edu/~seander/bithacks.html#IntegerMinOrMax
 real sum_minimum_vec(tint *a, tint *b, tint vec_dim)
 {
   tint i, c;  
-  real sum;
+  real sum=0;
   for (i=0;i<vec_dim;i++) {
         c = b[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // min(x, y)
+        //printf("%d, %d, %d\n", a[i],b[i],c);
         sum += c;
   }
+  return sum;
 }
 
 real sum_maximum_vec(tint *a, tint *b, tint vec_dim)
 {
   tint i, c;  
-  real sum;  
+  real sum=0;  
   for (i=0;i<vec_dim;i++) {
-        c = b[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // max(x, y)
+        c = a[i] ^ ((a[i] ^ b[i]) & -(a[i] < b[i])); // max(x, y)
         sum += c;
   }
+  return sum;
 }
 
 real get_non_zeros_pair(tint *a, tint *b, tint vec_dim)
 {
     real sum = 0;
     tint i;
-    for (i=0;i<vec_dim;i++)
-      if (a[i]>0 && b[i]>0)
-           sum += a[i]+b[i];
+    for (i=0;i<vec_dim;i++) {
+        if (a[i]>0 && b[i]>0)
+            sum += a[i]+b[i];
+    }
+    return sum;
 }
 
 tint get_non_zeros_single(tint *a, tint vec_dim)
@@ -100,10 +110,14 @@ real vec_dot(tint *a, tint *b, tint vec_dim)
 int main(void)
 {
     
-    tint data[N][D] = {{1,2,3,4,5},{6,7,8,9,10},{7,8,9,10,11}};
+    //tint data[N][D] = {{1,2,3,4,5},{6,7,8,9,10},{7,8,9,10,11}};
+    //tint data[N][D] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
+    tint data[N][D] = {{1,0,3,0,5},{6,7,0,9,10},{11,0,13,14,0}};
     tint data_jac[N][D]={0};
     tint cmb[C][2]={{0,1},{0,2},{1,2}};
     real data_sum[N], a_sum, b_sum, one_data_norm[N], one_an, one_bn, result;
+    real dist_gen_jac, dist_jac, denomenator_wu, dist_wu; 
+    real numerator_sarika, denomenator_sarika, dist_sarika;
 
     tint i, j, idx_a, idx_b;
     tint *a, *b, *a_jac, *b_jac;
@@ -134,14 +148,39 @@ int main(void)
 
         //vec_add(a, b, summed_array, D);
         numerator_jac = sum_minimum_vec(a_jac, b_jac, D);
+
         denomenator_jac = sum_maximum_vec(a_jac, b_jac, D);
+        //printf("numerator_jac=%f\n",numerator_jac);
+        //printf("denomenator_jac=%f\n",denomenator_jac);
         numerator_gen_jac = sum_minimum_vec(a, b, D);
         denomenator_gen_jac = sum_maximum_vec(a, b, D);
+        
+        //printf("numerator_gen_jac=%f\n",numerator_gen_jac);
+        //printf("denomenator_gen_jac=%f\n",denomenator_gen_jac);
+        
         num_sim = get_non_zeros_pair(a, b, D);
+        //printf("num_sim=%f\n",num_sim);
+        
         one_an = one_data_norm[idx_a];
         one_bn = one_data_norm[idx_b];
         result = 1.0 - vec_dot(a,b,D)*one_an*one_bn;
-            
+        
+        dist_gen_jac = 1.0-numerator_gen_jac/denomenator_gen_jac;
+        dist_jac = 1.0-numerator_jac/denomenator_jac;
+        
+        denomenator_wu = MIN(denomenator_gen_jac,MAX(a_sum,b_sum));
+        dist_wu = 1.0-numerator_gen_jac/denomenator_wu;
+        
+        numerator_sarika = num_sim;
+        denomenator_sarika = a_sum+b_sum;
+        dist_sarika = 1.0-numerator_sarika/denomenator_sarika;
+        
+        printf("normal = %f\n", dist_jac);
+        printf("generalised =%f\n", dist_gen_jac);
+        printf("sarika =%f\n", dist_sarika);
+        printf("wu =%f\n", dist_wu);
+        printf("result = %f\n", result);
+        
         //non_zeros = (a >0) & (b > 0)
         //summed_array = a + b
         //
@@ -151,5 +190,15 @@ int main(void)
         //denomenator_gen_jac =np.sum(np.maximum(a,b))
         //num_sim = np.sum(summed_array[non_zeros])
         //result = 1 - spatial.distance.cosine(a, b)
+        
+        //dist_gen_jac=1.0-(float(numerator_gen_jac)/float(denomenator_gen_jac))                    
+        //dist_jac=1.0-(float(numerator_jac)/float(denomenator_jac))
+        //
+        //denomenator_wu = min(denomenator_gen_jac,max(a_sum,b_sum) )
+        //dist_wu = 1.0-(float(numerator_gen_jac)/float(denomenator_wu))
+        //
+        //numerator_sarika = num_sim
+        //denomenator_sarika = a_sum+b_sum
+        //dist_sarika = 1.0-(float(numerator_sarika)/float(denomenator_sarika))
     }
 }
