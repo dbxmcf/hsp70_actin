3dd9982a088dc3206a4a487555139c7b7d29f2a2
Feng Chen <fchen3@gmail.com>
2019-01-29 19:09:22 -0600

    some cleanups

--- hdf5t/ph5_cmb_blks.c
+++ hdf5t/ph5_cmb_blks.c
@@ -802,10 +802,10 @@ phdf5readAll(char *filename)
     //printf("%5d",data_array1[3][3]);
     //dataset_fill(start, count, stride, &data_origin1[0][0]);
     //MESG("data_array initialized");
-    if (verbose){
-        MESG("data_array created");
-        dataset_print(start, count, stride, &data_array1[0][0]);
-    }
+    //if (verbose){
+    //    MESG("data_array created");
+    //    dataset_print(start, count, stride, &data_array1[0][0]);
+    //}
 //
     ///* set up the collective transfer properties list */
     xfer_plist = H5Pcreate (H5P_DATASET_XFER);
@@ -830,11 +830,6 @@ phdf5readAll(char *filename)
         printf("\n");
     }
 
-    free_dynamic_2d_array(data_array1);
-//
-    ///* verify the read data with original expected data */
-    //ret = dataset_vrfy(start, count, stride, &data_array1[0][0], &data_origin1[0][0]);
-    //assert(ret != FAIL);
 
     /* release all temporary handles. */
     /* Could have used them for dataset2 but it is cleaner */
@@ -843,70 +838,17 @@ phdf5readAll(char *filename)
     H5Sclose(mem_dataspace);
     H5Pclose(xfer_plist);
 
- //   /* Dataset2: each process takes a block of rows. */
- //   slab_set(start, count, stride, BYROW);
- //   if (verbose)
- //       printf("start[]=(%lu,%lu), count[]=(%lu,%lu), total datapoints=%lu\n",
- //       (unsigned long)start[0], (unsigned long)start[1],
- //           (unsigned long)count[0], (unsigned long)count[1],
- //           (unsigned long)(count[0]*count[1]));
-//
- //   /* create a file dataspace independently */
- //   file_dataspace = H5Dget_space (dataset1);
- //   assert(file_dataspace != FAIL);
- //   MESG("H5Dget_space succeed");
- //   ret=H5Sselect_hyperslab(file_dataspace, H5S_SELECT_SET, start, stride,
-//	    count, NULL);
- //   assert(ret != FAIL);
- //   MESG("H5Sset_hyperslab succeed");
-//
- //   /* create a memory dataspace independently */
- //   mem_dataspace = H5Screate_simple (SPACE1_RANK, count, NULL);
- //   assert (mem_dataspace != FAIL);
-//
- //   /* fill dataset with test data */
- //   dataset_fill(start, count, stride, &data_origin1[0][0]);
- //   MESG("data_array initialized");
- //   if (verbose){
-//	MESG("data_array created");
-//	dataset_print(start, count, stride, &data_array1[0][0]);
- //   }
-//
- //   /* set up the collective transfer properties list */
- //   xfer_plist = H5Pcreate (H5P_DATASET_XFER);
- //   assert(xfer_plist != FAIL);
- //   ret=H5Pset_dxpl_mpio(xfer_plist, H5FD_MPIO_COLLECTIVE);
- //   assert(ret != FAIL);
- //   MESG("H5Pcreate xfer succeed");
-//
- //   /* read data independently */
- //   ret = H5Dread(dataset2, H5T_NATIVE_INT, mem_dataspace, file_dataspace,
-//	    xfer_plist, data_array1);
- //   assert(ret != FAIL);
- //   MESG("H5Dread succeed");
-//
- //   /* verify the read data with original expected data */
- //   ret = dataset_vrfy(start, count, stride, &data_array1[0][0], &data_origin1[0][0]);
- //   assert(ret != FAIL);
-//
- //   /* release all temporary handles. */
- //   H5Sclose(file_dataspace);
- //   H5Sclose(mem_dataspace);
- //   H5Pclose(xfer_plist);
-
-
-    /*
+     /*
      * All reads completed.  Close datasets collectively
      */
     ret=H5Dclose(dataset1);
     assert(ret != FAIL);
     MESG("H5Dclose1 succeed");
- //   ret=H5Dclose(dataset2);
- //   assert(ret != FAIL);
- //   MESG("H5Dclose2 succeed");
 
     /* close the file collectively */
     H5Fclose(fid1);
+
+    free_dynamic_2d_array(data_array1);
 }
 
 /*
