bd1a71f7af2504fadde982b88f76b932f6625f4a
Feng Chen <fchen3@gmail.com>
2019-01-30 16:32:26 -0600

    some clean-ups

--- hdf5t/gen_pair_combinations.c
+++ hdf5t/gen_pair_combinations.c
@@ -3,23 +3,6 @@
 #include "ncr.h"
 #include "dynamic_2d_array.h"
 
-
-// The main function that prints all combinations of size r 
-// in arr[] of size n. This function mainly uses combination_util() 
-//void printCombination(int arr[], int n, int r) 
-//{ 
-//	// A temporary array to store all combination one by one 
-//	int i,data[r],idx;
-//	int **cmbs=NULL;
-//	int num_cmb=0, idx_cmb=0;
-//	num_cmb = ncr(5,3);
-//	printf("num_cmb=%d\n",num_cmb);
-//	// Print all combination using temprary array 'data[]' 
-//	cmbs = allocate_dynamic_2d_array(num_cmb,2);
-//	combination_util(arr, data, 0, n-1, 0, r, cmbs, num_cmb, &idx_cmb);
-//	free_dynamic_2d_array(cmbs);
-//} 
-
 /* arr[] ---> Input Array 
 data[] ---> Temporary array to store current combination 
 start & end ---> Staring and Ending indexes in arr[] 
@@ -32,7 +15,6 @@ void combination_util(int n, int **cmbs) //, int **cmbs, int num_cmb)
 	for (i=0;i<n;i++)
 	{ 
         for (j=i+1; j<n; j++) {
-			//printf("%d %d\n", i, j); 
 			cmbs[idx][0]=i;
             cmbs[idx][1]=j;
 			idx++;
